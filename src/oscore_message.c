coap-security/liboscore

commit e7ef9946a56e5ded66646f10d397189f8490c03c

protected message: code simplifcations

src/oscore_message.c
@@ -181,11 +181,8 @@ bool oscore_msg_protected_optiter_next(
     }
 
     // Determine next option
-    bool next_is_inner = false;
+    bool next_is_inner;
     if (iter->backend_exhausted) {
-        if (iter->inner_peeked_value == NULL) {
-            return false;
-        }
         next_is_inner = true;
     } else if (iter->inner_peeked_value != NULL) {
         // Return options ordered by option number

commit e3f21354162099f1f1a5aad2181e4bc54b0878fe

protected message: Fix uninitialized value
Fixes bug introduced in e7ef9946, thanks Marco for spotting it.

src/oscore_message.c
@@ -184,7 +184,9 @@ bool oscore_msg_protected_optiter_next(
     bool next_is_inner;
     if (iter->backend_exhausted) {
         next_is_inner = true;
-    } else if (iter->inner_peeked_value != NULL) {
+    } else if (iter->inner_peeked_value == NULL) {
+        next_is_inner = false;
+    } else {
         // Return options ordered by option number
         next_is_inner = iter->inner_peeked_optionnumber <
                 iter->backend_peeked_optionnumber;