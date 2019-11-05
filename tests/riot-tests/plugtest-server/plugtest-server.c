e08a0b84d589c4fced4bbedaadc6fb655c459763
chrysn <chrysn@fsfe.org>
2019-11-05 13:34:45 +0100

    implement plugtest server responses

--- tests/riot-tests/plugtest-server/plugtest-server.c
+++ tests/riot-tests/plugtest-server/plugtest-server.c
@@ -50,13 +50,21 @@ static ssize_t _hello(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx)
     return resp_len;
 }
 
+#define SENDER_KEY {50, 136, 42, 28, 97, 144, 48, 132, 56, 236, 152, 230, 169, 50, 240, 32, 112, 143, 55, 57, 223, 228, 109, 119, 152, 155, 3, 155, 31, 252, 28, 172}
+#define RECIPIENT_KEY {213, 48, 30, 177, 141, 6, 120, 73, 149, 8, 147, 186, 42, 200, 145, 65, 124, 137, 174, 9, 223, 74, 56, 85, 170, 0, 10, 201, 255, 243, 135, 81}
+#define COMMON_IV {100, 240, 189, 49, 77, 75, 224, 60, 39, 12, 43, 28}
+
 // Having those static is OK here because the gcoap thread will only process messages one at a time
 static struct oscore_context_primitive primitive = {
     .aeadalg = 24,
-    .common_iv = "d\xf0\xbd" "1MK\xe0<'\x0c+\x1c",
+    .common_iv = COMMON_IV,
 
     .recipient_id_len = 0,
-    .recipient_key = "\xd5" "0\x1e\xb1\x8d\x06xI\x95\x08\x93\xba*\xc8\x91" "A|\x89\xae\t\xdfJ8U\xaa\x00\n\xc9\xff\xf3\x87Q",
+    .recipient_key = RECIPIENT_KEY,
+
+    .sender_id_len = 1,
+    .sender_id = "\x01",
+    .sender_key = SENDER_KEY,
 };
 static oscore_context_t secctx = {
     .type = OSCORE_CONTEXT_PRIMITIVE,
@@ -85,11 +93,16 @@ static ssize_t _oscore(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx)
         goto error;
     }
 
-    // FIXME: THis is short-cutting through a lookup process that should
-    // actually be there to find the right secctx from the header
+    // FIXME: this should be in a dedicated parsed_pdu_to_oscore_msg_native_t process
+    // (and possibly foolishly assuming that there is a payload marker)
+    pdu->payload --;
+    pdu->payload_len ++;
+    oscore_msg_native_t pdu_read = { .pkt = pdu };
 
     oscore_msg_protected_t incoming_decrypted;
-    oscerr = oscore_unprotect_request(pdu, &incoming_decrypted, header, &secctx, &request_id);
+    // FIXME: THis is short-cutting through a lookup process that should
+    // actually be there to find the right secctx from the header
+    oscerr = oscore_unprotect_request(pdu_read, &incoming_decrypted, header, &secctx, &request_id);
 
     if (oscerr != OSCORE_UNPROTECT_REQUEST_OK) {
         if (oscerr == OSCORE_UNPROTECT_REQUEST_DUPLICATE) {
@@ -121,16 +134,62 @@ static ssize_t _oscore(coap_pkt_t *pdu, uint8_t *buf, size_t len, void *ctx)
 
     // Anything we were trying to learn from the incoming message needs to be
     // copied to the stack by now.
-    pdu = oscore_release_unprotected(incoming_decrypted);
+    oscore_msg_native_t pdu_read_out = oscore_release_unprotected(&incoming_decrypted);
+
+    assert(pdu_read_out.pkt == pdu);
 
     gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
 
-    // For now as we don't parse the above yet, the next easiest step would be
-    // to send back a protected error message ... can't do that yet.
-    errormessage = "Can't respond meaningfully yet";
-    goto error;
+    enum oscore_prepare_result oscerr2;
+    oscore_msg_native_t pdu_write = { .pkt = pdu };
+    oscore_msg_protected_t outgoing_plaintext;
+    oscerr2 = oscore_prepare_response(pdu_write, &outgoing_plaintext, &secctx, &request_id);
+    if (oscerr2 != OSCORE_PREPARE_OK) {
+        errormessage = "Context not ready";
+        goto error;
+    }
 
-    return 0;
+    // Set outer options: none
+
+    // Set code
+    oscore_msg_protected_set_code(&outgoing_plaintext, COAP_CODE_CONTENT);
+
+    // Set inner options
+    oscore_msgerr_protected_t oscerr3;
+    oscerr3 = oscore_msg_protected_append_option(&outgoing_plaintext, 12, (uint8_t*)"", 0);
+    if (oscore_msgerr_protected_is_error(oscerr3)) {
+        errormessage = "Failed to add content format";
+        goto error;
+    }
+
+    uint8_t *payload;
+    size_t payload_length;
+    size_t printed = 0;
+    oscerr3 = oscore_msg_protected_map_payload(&outgoing_plaintext, &payload, &payload_length);
+    if (oscore_msgerr_protected_is_error(oscerr3)) {
+        errormessage = "Failed to map message";
+        goto error;
+    }
+
+    printed = snprintf((char*)payload, payload_length, "Hello World!");
+
+    oscerr3 = oscore_msg_protected_trim_payload(&outgoing_plaintext, printed);
+    if (oscore_msgerr_protected_is_error(oscerr3)) {
+        errormessage = "Failed to trim message";
+        goto error;
+    }
+
+    enum oscore_finish_result oscerr4;
+    oscore_msg_native_t pdu_write_out;
+    oscerr4 = oscore_encrypt_message(&outgoing_plaintext, &pdu_write_out);
+    if (oscerr4 != OSCORE_FINISH_OK) {
+        errormessage = "Error finishing";
+        goto error;
+    }
+    assert(pdu == pdu_write_out.pkt);
+
+    // FIXME we'll have to pick that from pdu, or make the oscore_msg_native_t enriched by a length
+    return (pdu->payload - buf) + pdu->payload_len;
 
 error:
     // FIXME do error coes right
