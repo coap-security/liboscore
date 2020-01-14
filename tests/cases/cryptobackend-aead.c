36b10f0e329d9435ef0f15857559daf7086283f8
chrysn <chrysn@fsfe.org>
2020-01-14 15:39:01 +0100

    tests: Run both ChaCha and AES-CCM
    This will break with the current build system on both native and RIOT

--- tests/cases/cryptobackend-aead.c
+++ tests/cases/cryptobackend-aead.c
@@ -6,32 +6,54 @@
 #include <oscore_native/crypto.h>
 
 // keys happen to be derived as in RFC8613 C.1.1, but with ChaCha20/Poly1305
-#define SENDER_KEY {213, 48, 30, 177, 141, 6, 120, 73, 149, 8, 147, 186, 42, 200, 145, 65, 124, 137, 174, 9, 223, 74, 56, 85, 170, 0, 10, 201, 255, 243, 135, 81}
-#define RECIPIENT_KEY {50, 136, 42, 28, 97, 144, 48, 132, 56, 236, 152, 230, 169, 50, 240, 32, 112, 143, 55, 57, 223, 228, 109, 119, 152, 155, 3, 155, 31, 252, 28, 172}
-#define COMMON_IV {100, 240, 189, 49, 77, 75, 224, 60, 39, 12, 43, 28}
+#define CHACHA_SENDER_KEY {213, 48, 30, 177, 141, 6, 120, 73, 149, 8, 147, 186, 42, 200, 145, 65, 124, 137, 174, 9, 223, 74, 56, 85, 170, 0, 10, 201, 255, 243, 135, 81}
+#define CHACHA_COMMON_IV {100, 240, 189, 49, 77, 75, 224, 60, 39, 12, 43, 28}
+
+// sender ID 03, secret "correct horse battery staple"
+#define AESCCM_SENDER_KEY {119, 148, 122, 227, 70, 87, 45, 192, 116, 50, 219, 69, 190, 126, 239, 172}
+#define AESCCM_COMMON_IV {11, 216, 46, 234, 187, 151, 85, 149, 207, 126, 20, 175, 68}
 
 const size_t max_tag_length = 16;
 
-static const uint8_t key[] = SENDER_KEY;
+struct testdata {
+    oscore_crypto_aeadalg_t alg;
+    const uint8_t *key;
+    const uint8_t *nonce;
+};
+
+static const uint8_t chacha_key[] = CHACHA_SENDER_KEY;
 // This happens to be the actually used IV for PIV 0 and the empty sender ID
-static const uint8_t nonce[] = COMMON_IV;
+static const uint8_t chacha_nonce[] = CHACHA_COMMON_IV;
+static struct testdata chacha_data = {
+    .alg = 24,
+    .key = chacha_key,
+    .nonce = chacha_nonce,
+};
+
+static const uint8_t aesccm_key[] = AESCCM_SENDER_KEY;
+static const uint8_t aesccm_nonce[] = AESCCM_COMMON_IV;
+static struct testdata aesccm_data = {
+    .alg = 10,
+    .key = aesccm_key,
+    .nonce = aesccm_nonce,
+};
 
 const char message[] = "The quick brown fox jumps over the lazy dog.";
 
-int testmain(int introduce_error)
+int test_with(struct testdata *data, int introduce_error)
 {
     oscore_cryptoerr_t err;
 
     uint8_t arena[sizeof(message) + max_tag_length];
     memcpy(arena, message, sizeof(message));
 
-    oscore_crypto_aeadalg_t chacha;
-    err = oscore_crypto_aead_from_number(&chacha, 24);
+    oscore_crypto_aeadalg_t alg;
+    err = oscore_crypto_aead_from_number(&alg, data->alg);
     if (oscore_cryptoerr_is_error(err)) {
         return 1;
     }
 
-    size_t tag_length = oscore_crypto_aead_get_taglength(chacha);
+    size_t tag_length = oscore_crypto_aead_get_taglength(alg);
     if (tag_length > max_tag_length) {
         return 2;
     }
@@ -42,11 +64,11 @@ int testmain(int introduce_error)
     oscore_crypto_aead_encryptstate_t encstate;
     err = oscore_crypto_aead_encrypt_start(
             &encstate,
-            chacha,
+            alg,
             sizeof(aad),
             sizeof(message),
-            nonce,
-            key
+            data->nonce,
+            data->key
             );
     if (oscore_cryptoerr_is_error(err)) {
         return 30;
@@ -64,11 +86,11 @@ int testmain(int introduce_error)
     oscore_crypto_aead_decryptstate_t decstate;
     err = oscore_crypto_aead_decrypt_start(
             &decstate,
-            chacha,
+            alg,
             sizeof(aad),
             sizeof(message),
-            nonce,
-            key
+            data->nonce,
+            data->key
             );
     if (oscore_cryptoerr_is_error(err)) return 40;
     err = oscore_crypto_aead_decrypt_feed_aad(&decstate, aad, 3);
@@ -81,4 +103,14 @@ int testmain(int introduce_error)
     assert(memcmp(message, arena, sizeof(message)) == 0);
 
     return 0;
+}
+
+int testmain(int introduce_error)
+{
+    int ret;
+    ret = test_with(&chacha_data, introduce_error == 1);
+    if (ret != 0)
+        return ret;
+    ret = test_with(&aesccm_data, introduce_error > 1);
+    return ret;
 }
