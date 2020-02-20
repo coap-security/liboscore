58d6510095ddc54ab372d8c7f164faf53d434dfd
chrysn <chrysn@fsfe.org>
2020-02-20 10:10:53 +0100

    demo: Emulate persistence on the native port

--- tests/riot-tests/plugtest-server/persistence.c
+++ tests/riot-tests/plugtest-server/persistence.c
@@ -1,6 +1,98 @@
 #include <string.h>
-#include <periph/flashpage.h>
 #include "persistence.h"
+
+#ifdef BOARD_NATIVE
+
+#include <sys/stat.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+static struct persisted_data *allocated;
+static char *filename;
+
+/** Emulating flash storage by creating a persistence.flash (or ${PERSISTENCE_FILENAME}) file.
+ *
+ * Versioning conflicts are avoided by discarding the file if it's older than the own binary.
+ */
+bool persistence_init(struct persisted_data **data)
+{
+    allocated = malloc(sizeof(struct persisted_data));
+    *data = allocated;
+
+    filename = getenv("PERSISTENCE_FILENAME");
+    if (filename == NULL)
+        filename = "persistence.flash";
+
+    struct stat own_binary;
+    struct stat old_file;
+
+    if (stat("/proc/self/exe", &own_binary)) {
+        perror("Failed to stat own file, discarding any persistence data");
+        return false;
+    }
+    if (stat(filename, &old_file)) {
+        // Absence of the file will not be announced, it's not an error but expected
+        if (errno != ENOENT)
+            perror("Failed to stat persistence file, discarding any persistence data");
+        return false;
+    }
+
+    if (own_binary.st_mtime > old_file.st_mtime) {
+        fprintf(stderr, "Persistence fileis older than own binary, discarding persisted state on first write.\n");
+        return false;
+    }
+
+    int f = open(filename, O_RDONLY);
+    if (f <= 0) {
+        perror("Failed to open persistence file");
+        return false;
+    }
+
+    ssize_t bytesread = read(f, allocated, sizeof(struct persisted_data));
+    if (bytesread != sizeof(struct persisted_data)) {
+        // It's small enough that it's very very very unlikely that we'd need
+        // to keep reading after a short read.
+        fprintf(stderr, "Failed to read complete persisted data from file.\n");
+        return false;
+    }
+
+    return true;
+}
+
+/** Write data from allocated memory to the file.
+ *
+ * Like the below flash-based implementation, this will not do any tempfile
+ * stuff to avoid losing data; if the program is interrupted right while it's
+ * writing, bad luck.
+ */
+void persistence_commit(void)
+{
+    int f = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+    if (f <= 0) {
+        perror("Failed to open persistence file; aborting");
+        abort();
+    }
+
+    if (write(f, allocated, sizeof(struct persisted_data)) != sizeof(struct persisted_data)) {
+        fprintf(stderr, "Failed to write all data to persistence file; aborting.\n");
+        abort();
+    }
+
+    if (fsync(f) != 0) {
+        perror("Failed to sync persistence data; aborting");
+        abort();
+    }
+
+    if (close(f) != 0) {
+        perror("Failed to close persistence file)");
+        abort();
+    }
+}
+
+#else // non-native boards
+
+#include <periph/flashpage.h>
 
 // Don't access directly, as the compiler would infer from its const-ness that
 // its null content may be propagated
@@ -43,3 +135,5 @@ void persistence_commit(void)
     while (ok != FLASHPAGE_OK);
 
 }
+
+#endif
