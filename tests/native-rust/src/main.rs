8022d363b108add09e6a34c378e37861baad1abd
chrysn <chrysn@fsfe.org>
2022-12-17 19:59:52 +0100

    Rust: minor test refactoring

--- tests/native-rust/src/main.rs
+++ tests/native-rust/src/main.rs
@@ -4,20 +4,31 @@ extern crate liboscore_msgbackend;
 
 include!(concat!(env!("OUT_DIR"), "/testmain-list.rs"));
 
+fn run_c_test(name: &'static str, tmf: unsafe extern "C" fn(i32) -> i32) -> Result<(), &'static str> {
+    let mut first_error = Ok(());
+
+    println!("Running test case {} without introducing errors...", name);
+    let result = unsafe { tmf(0) };
+    println!("Test ran, result was {}", result);
+    if result != 0 && first_error.is_ok() {
+        first_error = Err(name);
+    }
+    println!("Running test case {} and introducing errors...", name);
+    let result = unsafe { tmf(1) };
+    println!("Test ran, result was {}", result);
+    if result == 0 && first_error.is_ok() {
+        first_error = Err(name);
+    }
+
+    first_error
+}
+
 fn main() -> Result<(), &'static str> {
     let mut first_error = Ok(());
-    for (name, tmf) in TESTMAINS {
-        println!("Running test case {} without introducing errors...", name);
-        let result = unsafe { tmf(0) };
-        println!("Test ran, result was {}", result);
-        if result != 0 && first_error.is_ok() {
-            first_error = Err(name);
-        }
-        println!("Running test case {} and introducing errors...", name);
-        let result = unsafe { tmf(1) };
-        println!("Test ran, result was {}", result);
-        if result == 0 && first_error.is_ok() {
-            first_error = Err(name);
+    for (name, test_main_function) in TESTMAINS {
+        let result = run_c_test(name, test_main_function);
+        if first_error.is_ok() {
+            first_error = result;
         }
     }
     first_error
